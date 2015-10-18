3998ece44263955eb4886a9559179ce5949e8bae
叶冠鸿 <guanhong.ygh@alibaba-inc.com>
2015-10-18 17:39:17 +0800

    初始化问题解决

--- rpc/commons/src/main/java/com/jeremie/spring/rpc/dto/RPCdto.java
+++ rpc/commons/src/main/java/com/jeremie/spring/rpc/dto/RPCdto.java
@@ -1,7 +1,45 @@
 package com.jeremie.spring.rpc.dto;
 
+import java.io.Serializable;
+
 /**
  * @author guanhong 15/10/18 下午1:33.
  */
-public class RPCdto {
+public class RPCDto implements Serializable{
+    private String destClazz;
+    private String method;
+    private Class[] paramsType;
+    private Object[] params;
+
+    public String getDestClazz() {
+        return destClazz;
+    }
+
+    public void setDestClazz(String destClazz) {
+        this.destClazz = destClazz;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
+
+    public void setParams(Object[] params) {
+        this.params = params;
+    }
+
+    public Class[] getParamsType() {
+        return paramsType;
+    }
+
+    public void setParamsType(Class[] paramsType) {
+        this.paramsType = paramsType;
+    }
 }
