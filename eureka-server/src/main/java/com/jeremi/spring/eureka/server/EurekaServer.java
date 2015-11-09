c1e28664cc2c6bf277e11b7fb12db3e8d8b8bbb6
叶冠鸿 <guanhong.ygh@alibaba-inc.com>
2015-11-09 15:52:16 +0800

    添加eureka server

---
+++ eureka-server/src/main/java/com/jeremi/spring/eureka/server/EurekaServer.java
@@ -0,0 +1,22 @@
+package com.jeremi.spring.eureka.server;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author guanhong 15/11/9 下午1:55.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaServer
+@RestController
+public class EurekaServer {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
+    }
+}
