7e021007f40e3dacbf5bead137da6c91e0babee2
叶冠鸿 <guanhong.ygh@alibaba-inc.com>
2015-12-01 15:58:57 +0800

    初步切换rpc初始化方式,多服务端尚未完成

--- rpc/client/src/main/java/com/jeremie/spring/rpc/config/RPCConfiguration.java
+++ rpc/client/src/main/java/com/jeremie/spring/rpc/config/RPCConfiguration.java
@@ -1,10 +1,15 @@
 package com.jeremie.spring.rpc.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author guanhong 15/10/24 下午9:32.
  */
+@Component
 @ConfigurationProperties("com.jeremie.rpc")
 public class RpcConfiguration {
 
@@ -12,7 +17,7 @@ public class RpcConfiguration {
     private int defaultPort;
     private int defaultHttpport;
     private int defaultNioClientPort;
-    private String connectionMethod;
+    private List<ServiceConfig> services = new ArrayList<>();
 
     public String getDefaultIp() {
         return defaultIp;
@@ -46,12 +51,11 @@ public class RpcConfiguration {
         this.defaultNioClientPort = defaultNioClientPort;
     }
 
-    public String getConnectionMethod() {
-        return connectionMethod;
+    public List<ServiceConfig> getServices() {
+        return services;
     }
 
-    public void setConnectionMethod(String connectionMethod) {
-        this.connectionMethod = connectionMethod;
+    public void setServices(List<ServiceConfig> services) {
+        this.services = services;
     }
-
 }
