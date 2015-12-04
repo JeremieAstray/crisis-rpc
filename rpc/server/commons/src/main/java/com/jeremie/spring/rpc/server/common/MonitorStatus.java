0bfcd3c05cf012c6b830d7ebc586e62e3bb211ee
jeremie_astray <5490584810@163.com>
2015-12-05 01:32:16 +0800

    添加Remote参数

--- rpc/server/commons/src/main/java/com/jeremie/spring/rpc/server/common/MonitorStatus.java
+++ rpc/server/commons/src/main/java/com/jeremie/spring/rpc/server/common/MonitorStatus.java
@@ -10,5 +10,27 @@ public class MonitorStatus {
 
     public static int ConnectCount = 0;
     public static Map<String, MethodStatus> methodStatusMap = new ConcurrentHashMap<>();
+    public Remote remote;
 
+    public enum Remote{
+        bio,http,mina,netty,nio;
+
+        @Override
+        public String toString(){
+            switch (this){
+                case bio:
+                    return "bio";
+                case http:
+                    return "http";
+                case mina:
+                    return "mina";
+                case netty:
+                    return "netty";
+                case nio:
+                    return "nio";
+                default:
+                    return null;
+            }
+        }
+    }
 }
