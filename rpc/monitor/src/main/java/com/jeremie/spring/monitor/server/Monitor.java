3b8d2622aabbfe3caf5504daad5ec5f2740faca0
叶冠鸿 <guanhong.ygh@alibaba-inc.com>
2015-12-16 16:51:43 +0800

    修改maven依赖,将monitor分拆

--- rpc/monitor/src/main/java/com/jeremie/spring/monitor/server/Monitor.java
+++ rpc/monitor/src/main/java/com/jeremie/spring/monitor/server/Monitor.java
@@ -1,8 +1,7 @@
-package com.jeremie.spring.eureka.server;
+package com.jeremie.spring.monitor.server;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,12 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 /**
  * @author guanhong 15/11/9 下午1:55.
  */
-@ComponentScan(basePackages = {"com.jeremie.spring.eureka"})
+@ComponentScan(basePackages = {"com.jeremie.spring.monitor"})
 @EnableAutoConfiguration
-@EnableEurekaServer
 @RestController
-public class EurekaServer extends WebMvcConfigurerAdapter {
+public class Monitor extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
+        new SpringApplicationBuilder(Monitor.class).web(true).run(args);
     }
 }
