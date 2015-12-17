1d8408124fe2b0711a7c3a102349dc8897d8a7ed
叶冠鸿 <guanhong.ygh@alibaba-inc.com>
2015-12-17 16:23:18 +0800

    修改依赖

--- rpc/monitor/src/main/java/com/jeremie/spring/monitor/server/MonitorServer.java
+++ rpc/monitor/src/main/java/com/jeremie/spring/monitor/server/MonitorServer.java
@@ -1,6 +1,5 @@
 package com.jeremie.spring.monitor.server;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.netflix.appinfo.InstanceInfo;
@@ -9,7 +8,6 @@ import com.netflix.discovery.shared.Application;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -21,9 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author guanhong 15/12/8 下午2:16.
@@ -45,7 +41,7 @@ public class MonitorServer {
             if (!"EUREKA-SERVER".equals(applicationName)) {
                 application.getInstances().forEach(instanceInfoList::add);
                 if (!result.containsKey(applicationName))
-                    result.put(applicationName,new JSONArray());
+                    result.put(applicationName, new JSONArray());
             }
         });
         instanceInfoList.forEach(instanceInfo -> {
