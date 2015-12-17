d74be68f61f0bbda08fecdcb15a92e3a478709f9
叶冠鸿 <guanhong.ygh@alibaba-inc.com>
2015-12-17 14:15:08 +0800

    提交monitor

---
+++ rpc/monitor/src/main/java/com/jeremie/spring/eureka/server/MonitorServer.java
@@ -0,0 +1,70 @@
+package com.jeremie.spring.eureka.server;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author guanhong 15/12/8 下午2:16.
+ */
+@RestController
+public class MonitorServer {
+    @Autowired
+    private EurekaClient discoverEurekaClient;
+
+    private Logger logger = Logger.getLogger(this.getClass());
+
+    @RequestMapping(path = "/monitor")
+    public String status() {
+        List<Application> applicationList = discoverEurekaClient.getApplications().getRegisteredApplications();
+        List<InstanceInfo> instanceInfoList = new ArrayList<>();
+        JSONObject result = new JSONObject();
+        applicationList.forEach(application -> {
+            String applicationName = application.getName();
+            if (!"EUREKA-SERVER".equals(applicationName)) {
+                application.getInstances().forEach(instanceInfoList::add);
+                if (!result.containsKey(applicationName))
+                    result.put(applicationName,new JSONArray());
+            }
+        });
+        instanceInfoList.forEach(instanceInfo -> {
+            try {
+                CloseableHttpClient httpClient = HttpClients.createDefault();
+                String url = "http://" + instanceInfo.getHostName() + ":" + instanceInfo.getPort() + "/rpcMonitor/rpcStatus";
+                HttpGet httpGet = new HttpGet(url);
+                HttpClientContext httpContext = new HttpClientContext();
+                HttpResponse response = httpClient.execute(httpGet, httpContext);
+                HttpEntity resultEntity = response.getEntity();
+                String re = EntityUtils.toString(resultEntity);
+                JSONObject jsonObject = JSONObject.parseObject(re);
+                jsonObject.put("host", instanceInfo.getIPAddr());
+                JSONArray jsonArray = (JSONArray) result.get(instanceInfo.getAppName());
+                jsonArray.add(jsonObject);
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            }
+        });
+        return result.toJSONString();
+    }
+}
---
+++ rpc/monitor/src/main/java/com/jeremie/spring/eureka/server/ServletInitializer.java
@@ -0,0 +1,16 @@
+package com.jeremie.spring.eureka.server;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * @author guanhong 15/9/11 下午8:37.
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Monitor.class);
+    }
+
+}
